d2f5ae93940f9f812790c85d7a90a5fa4d419419
Ann-Fox <fx8565@mail.ru>
2023-08-23 14:22:04 +0500

    add keyboard layout

--- src/components/CatalogItem.vue
+++ src/components/CatalogItem.vue
@@ -40,8 +40,20 @@ const addMe = () => {
       <div>{{ priceProduct }}</div>
     </div>
     <div class="action">
-      <button>
+      <button class="action__calc">
         <IconCalc class="icon"></IconCalc>
+        <div class="action__calc__keyboard">
+          <button class="keyboard_del">delete</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
       </button>
       <input type="number" :value="qty"/>
       <button @click="qty++">+</button>
@@ -67,11 +79,28 @@ grid-row-gap: 50px;
   grid-template-columns: 2fr 1fr;
   grid-column-gap: 40px;
 }
-
 .action {
   display: flex;
   justify-content: flex-start;
 }
+
+.action__calc {
+  position: relative;
+}
+
+.action__calc__keyboard {
+  position: absolute;
+  background: #00BD7E;
+  padding: 5px;
+  
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: repeat(4, 1fr);
+grid-column-gap: 5px;
+grid-row-gap: 5px;
+}
+
+.keyboard_del { grid-area: 4 / 1 / 5 / 4; }
 
 .action input {
   max-width: 50px;
